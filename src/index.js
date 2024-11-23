e36cee7dad9dac30e1a66c96e538a5ddfa2db64e
LirWe786 <primak2020@gmail.com>
2024-11-23 08:59:59 +0300

    update mobile version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
- const cursorCenter = document.querySelector('.cursor_center');
+const cursorCenter = document.querySelector('.cursor_center');
 const cursorAura = document.querySelector('.cursor_aura');
 const headerMenu = document.querySelector('.header_menu');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.close_menu');
+const contactBtn = document.querySelector('.contact-btn');
+let closeContactWindow = document.querySelector('.close-contact-window');
+let contactWindow = document.querySelector('.contact-window');
 import { data } from '../modules/module.js'
 
 
@@ -16,11 +19,7 @@ const appState = {
 
 
 
-// animations 
-function skillsAnimatation(element) {
-    element.classList.add('skills_divs_animatation');
-    element.style.opacity = 1;
-}
+
 function cursorMiove(event) {
     cursorCenter.style.top = event.pageY + 'px';
     cursorCenter.style.left = event.pageX + 'px';
@@ -38,41 +37,63 @@ function openMobileMenu() {
     }
 
 }
+function sendConfirmationToTelegram(message) {
+    const telegramBotToken = '7666439315:AAGHkuXvxzWZG7Npq1I8xwKv3X0sxa72pBM'; // Ваш токен бота
+    const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+    const chatId = '2040393675'; // ID чата
+
+    fetch(telegramApiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: message
+        })
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log('Уведомление успешно отправлено в Telegram!');
+            } else {
+                console.error('Ошибка при отправке уведомления в Telegram:', response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка при отправке запроса:', error);
+        });
+}
 // listeners
 window.addEventListener('mousemove', cursorMiove);
 headerMenu.addEventListener('click', openMobileMenu);
 closeMenu.addEventListener('click', openMobileMenu);
+contactBtn.addEventListener('click', () => {
+    contactWindow.style.display = 'flex';
+})
+closeContactWindow.addEventListener('click', () => {
+    contactWindow.style.display = 'none';
+})
 
 
+// Обработчик события для кнопки
+document.querySelector('.message-btn').addEventListener('click', () => {
+    const name = document.getElementById('nameInput').value;
+    const email = document.getElementById('emailInput').value;
+    const number = document.getElementById('numInput').value;
 
+    const message = `Имя: ${name}\nEmail: ${email}\nНомер: ${number}`;
 
-
-
-// document.addEventListener('scroll', () => {
-//     const languagesSkillsDiv = document.querySelector('.languagesSkillsDiv');
-//     const languagesElement = document.querySelector('.skills_languages');
-//     const rect = languagesElement.getBoundingClientRect();
-//     const windowHeight = window.innerHeight;
-
-//     if (rect.top >= 0 && rect.bottom <= windowHeight) {
-//         skillsAnimatation(languagesSkillsDiv);
-//     }
-// });
-// document.addEventListener('scroll', () => {
-//     const designElement = document.querySelector('.skills_design');
-//     const designSkillsDiv = document.querySelector('.designSkillsDiv');
-//     const rect = designElement.getBoundingClientRect();
-//     const windowHeight = window.innerHeight;
-
-//     if (rect.top >= 0 && rect.bottom <= windowHeight) {
-//         skillsAnimatation(designSkillsDiv);
-//     }
-// });
-document.addEventListener('scroll', () => {
-    skillsAnimatation()
-
-
+    // Отправка сообщения в Telegram
+    sendConfirmationToTelegram(message);
 });
-document.addEventListener('scroll', () => {
-  
-});
+
+// git init - иниц проекта
+// git add . - добавляет файлы в реп
+// git commit -m 'bug fix' - коммитит
+// git branch -M main - создает ветку main
+// git remote add origin ссылка на реп - создает коннект с репом
+// git push - отправляет на гитхаб
+
+
+
+
